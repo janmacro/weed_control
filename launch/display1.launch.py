b37e51995af9d160d3740514514ac9fbccf8ca63
Jan-Marco Haldemann <j.haldemann@proton.me>
2024-03-06 10:45:20 +0100

    Added second launch file

--- launch/display1.launch.py
+++ launch/display1.launch.py
@@ -1,13 +1,13 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    model_path = PathJoinSubstitution(['urdf', 'robot.urdf.xacro'])
+    model_path = PathJoinSubstitution(['urdf', 'robot1.urdf.xacro'])
     rviz_config_path = PathJoinSubstitution([FindPackageShare('weed_control'), 'rviz', 'urdf.rviz'])
     
     ld.add_action(IncludeLaunchDescription(
---
+++ launch/display2.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    model_path = PathJoinSubstitution(['urdf', 'robot2.urdf.xacro'])
+    rviz_config_path = PathJoinSubstitution([FindPackageShare('weed_control'), 'rviz', 'urdf.rviz'])
+    
+    ld.add_action(IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
+        launch_arguments={
+            'urdf_package': 'weed_control',
+            'urdf_package_path': model_path,
+            'rviz_config': rviz_config_path}.items()
+    ))
+
+    return ld
